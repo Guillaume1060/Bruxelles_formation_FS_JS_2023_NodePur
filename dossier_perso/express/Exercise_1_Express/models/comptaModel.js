2844adc4d6a5b6fadd1add9ef00cc6e7b6bc7b93
Guillaume <breyerguillaume@gmail.com>
2023-03-14 11:25:42 +0100

    update compta

--- dossier_perso/express/Exercise_1_Express/models/comptaModel.js
+++ dossier_perso/express/Exercise_1_Express/models/comptaModel.js
@@ -4,24 +4,33 @@ const factureModel = {
   getAll: () => {
     return factures;
   },
-
   getOne: (id) => {
     return factures.find((facture) => facture.id == id);
   },
-
-  update: (id) => {
+  createOne: (facture) => {
+    let id = factures.length + 1;
+    facture.id = id;
+    factures.push(facture);
+    return factures.filter((facture) => facture.paid == false);
+  },
+  update: (id, update) => {
     const facture = factures.find((facture) => facture.id == id);
+    if (update.amount !== undefined) {
+      facture.amount = update.amount;
+    }
+    if (update.date_echeance !== undefined) {
+      facture.date_echeance = update.date_echeance;
+    }
     return facture;
   },
-  createOne: (facture) => {
-    factures.push(facture);
-    // TODO revoir la méthod filter qui ne fonctionne pas bien
-    return factures.filter((facture) => facture.paid === false);
-  },
-
   delete: (id) => {
-    let facture = factures.find((facture) => facture.id == id);
-    factures.splice(facture, 1);
+    let facturePayee = factures.find((facture) => facture.id == id);
+    if (facturePayee.paid == true) {
+      throw new Error("la facture a déjà été payé");
+    } else {
+      facturePayee.paid = true;
+      return facturePayee;
+    }
   },
 };
 
